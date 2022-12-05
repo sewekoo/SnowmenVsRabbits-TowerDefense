93d1dc170f6727c66644512711d485444817b5eb
Roosa <Roosa>
2022-12-05 16:47:02 +0200

    Added information about prices for towers

--- src/sidebar.hpp
+++ src/sidebar.hpp
@@ -7,10 +7,7 @@
 class Sidebar {
 public:
  Sidebar() { 
-    /*sf::Texture basic;
-    sf::Texture hat;
-    sf::Texture scarf;
-    if (!basic.loadFromFile("pics/snowman_basic1.png")) {
+    /*if (!basic.loadFromFile("pics/snowman_basic1.png")) {
       std::cout << "not working" << std::endl;
     }
     if (!hat.loadFromFile("pics/snowman_hat1.png")) {
@@ -19,53 +16,111 @@ public:
     if (!scarf.loadFromFile("pics/snowman_scarf1.png")) {
       std::cout << "not working" << std::endl;
     }*/
-    tower1B.setFillColor(sf::Color::Blue);
-    tower1B.setPosition({1010, 100});
+    tower1B.setFillColor(sf::Color(0, 0, 255, 30));
+    tower1B.setOutlineColor(sf::Color::White);
+    tower1B.setOutlineThickness(3);
+    tower1B.setPosition({1014, 100});
     tower1B.setSize(sf::Vector2f(90, 90));
-    tower2B.setFillColor(sf::Color::Blue);
-    tower2B.setPosition({1010, 250});
+    tower2B.setFillColor(sf::Color(0, 0, 255, 30));
+    tower2B.setOutlineColor(sf::Color::White);
+    tower2B.setOutlineThickness(3);
+    tower2B.setPosition({1014, 235});
     tower2B.setSize(sf::Vector2f(90, 90));
-    tower3B.setFillColor(sf::Color::Blue);
-    tower3B.setPosition({1010, 400});
+    tower3B.setFillColor(sf::Color(0, 0, 255, 30));
+    tower3B.setOutlineColor(sf::Color::White);
+    tower3B.setOutlineThickness(3);
+    tower3B.setPosition({1014, 370});
     tower3B.setSize(sf::Vector2f(90, 90));
 
-    goButton.setFillColor(sf::Color::Red);
-    goButton.setPosition({960, 500});
-    goButton.setSize(sf::Vector2f(200, 150));
+    goButton.setFillColor(sf::Color(255, 75, 0, 100));
+    goButton.setPosition({980, 520});
+    goButton.setSize(sf::Vector2f(150, 100));
     goButtonText.setString("GO");
     CenterText({960, 500});
 
     tower1.setPosition({1010, 100});
-    tower2.setPosition({1010, 250});
-    tower3.setPosition({1010, 400});
+    tower2.setPosition({1010, 235});
+    tower3.setPosition({1010, 370});
 
     sidebar.setSize(sf::Vector2f(200, 650));
-    sidebar.setFillColor(sf::Color(255, 100, 0, 200));
-    sidebar.setOutlineColor(sf::Color::Black);
+    sidebar.setFillColor(sf::Color(255, 255, 255, 230));
+    sidebar.setOutlineColor(sf::Color(0, 0, 0, 30));
     sidebar.setOutlineThickness(3);
     sidebar.setPosition(960, 0);
 
     roundCount.setString("Round: 1 / 5");
-    roundCount.setPosition({970, 10});
+    roundCount.setPosition({970, 5});
 
-    line.setSize(sf::Vector2f(200, 3));
-    line.setFillColor(sf::Color::Black);
-    /*line.setOutlineColor(sf::Color::Black);
-    line.setOutlineThickness(3);*/
-    line.setPosition(960, 50);
+    wallet.setString("Wallet: ");
+    wallet.setPosition({970, 50});
+
+    basicPrice.setString("Price: x$");
+    basicPrice.setPosition({1025, 195});
+    basicPrice.setCharacterSize(12);
+    basicPrice.setFillColor(sf::Color::Black);
+
+    basicUpgrade.setString("Upgrade: x$");
+    basicUpgrade.setPosition({1020, 212});
+    basicUpgrade.setCharacterSize(12);
+    basicUpgrade.setFillColor(sf::Color::Black);
+
+    hatPrice.setString("Price: x$");
+    hatPrice.setPosition({1025, 330});
+    hatPrice.setCharacterSize(12);
+    hatPrice.setFillColor(sf::Color::Black);
+
+    hatUpgrade.setString("Upgrade: x$");
+    hatUpgrade.setPosition({1020, 347});
+    hatUpgrade.setCharacterSize(12);
+    hatUpgrade.setFillColor(sf::Color::Black);
+
+    scarfPrice.setString("Price: x$");
+    scarfPrice.setPosition({1025, 465});
+    scarfPrice.setCharacterSize(12);
+    scarfPrice.setFillColor(sf::Color::Black);
+
+    scarfUpgrade.setString("Upgrade: x$");
+    scarfUpgrade.setPosition({1020, 482});
+    scarfUpgrade.setCharacterSize(12);
+    scarfUpgrade.setFillColor(sf::Color::Black);
+
+    line1.setSize(sf::Vector2f(200, 3));
+    line1.setFillColor(sf::Color(0, 0, 0, 30));
+    line1.setPosition(960, 45);
+
+    line2.setSize(sf::Vector2f(200, 3));
+    line2.setFillColor(sf::Color(0, 0, 0, 30));
+    line2.setPosition(960, 90);
 }
 
 void SetFont(sf::Font &font) { 
     roundCount.setFont(font);
+    wallet.setFont(font);
     goButtonText.setFont(font);
+    basicPrice.setFont(font);
+    basicUpgrade.setFont(font);
+    hatPrice.setFont(font);
+    hatUpgrade.setFont(font);
+    scarfPrice.setFont(font);
+    scarfUpgrade.setFont(font);
 }
 
 void UpdateRoundCount(int round) { roundCount.setString("Round: " + std::to_string(round) + " / 5"); }
 
+void UpdateWallet(int amount) { wallet.setString("Wallet: " + std::to_string(amount) + "$"); }
+
 void drawTo(sf::RenderWindow &window) { 
     window.draw(sidebar);
     window.draw(roundCount);
-    window.draw(line);
+    window.draw(wallet);
+    window.draw(line1);
+    window.draw(line2);
+    window.draw(basicPrice);
+    window.draw(basicUpgrade);
+    window.draw(hatPrice);
+    window.draw(hatUpgrade);
+    window.draw(scarfPrice);
+    window.draw(scarfUpgrade);
     window.draw(tower1B);
     window.draw(tower2B);
     window.draw(tower3B);
@@ -93,9 +148,9 @@ bool MouseOnButton(sf::RenderWindow &window, sf::RectangleShape button) {
 }
 
 void CenterText(sf::Vector2f posOfGoButton) { 
-        float posX = (posOfGoButton.x + goButton.getLocalBounds().width / 6) -
+        float posX = (posOfGoButton.x + goButton.getLocalBounds().width / 2) -
                      (goButtonText.getLocalBounds().width / 2);
-        float posY = (posOfGoButton.y + goButton.getLocalBounds().height / 7) -
+        float posY = (posOfGoButton.y + goButton.getLocalBounds().height / 2) -
                      (goButtonText.getLocalBounds().height / 2);
         goButtonText.setPosition(posX, posY);
     }
@@ -103,7 +158,15 @@ void CenterText(sf::Vector2f posOfGoButton) {
 //private:
     sf::RectangleShape sidebar;
     sf::Text roundCount;
-    sf::RectangleShape line;
+    sf::Text wallet;
+    sf::Text basicPrice;
+    sf::Text basicUpgrade;
+    sf::Text hatPrice;
+    sf::Text hatUpgrade;
+    sf::Text scarfPrice;
+    sf::Text scarfUpgrade;
+    sf::RectangleShape line1;
+    sf::RectangleShape line2;
     sf::Sprite tower1;
     sf::Sprite tower2;
     sf::Sprite tower3;
