67f1fbd55e1d9f262c6236695f47b30ab1f004db
Severi Koivumaa <severi.koivumaa@aalto.fi>
2022-12-09 11:20:19 +0200

    Commented out range increase with tower upgrade

--- src/tower.cpp
+++ src/tower.cpp
@@ -1,19 +1,18 @@
 #include "tower.hpp"
 
 void Tower::Upgrade() {
-    // Increment level
-    level_ += 1;
+  // Increment level
+  level_ += 1;
 
-    // Increase value
-    value_ += upgradePrice_;
+  // Increase value
+  value_ += upgradePrice_;
 
-    // Increase damage
-    damage_ = 1.5 * damage_;
+  // Increase damage
+  damage_ = 1.5 * damage_;
 
-    // Increase range
-    range_ = range_ + 1;
+  // Increase range
+  // range_ = range_ + 1;
 
-    // Increase upgrade price
-    upgradePrice_ = 2 * upgradePrice_;
-
+  // Increase upgrade price
+  upgradePrice_ = 2 * upgradePrice_;
 }
